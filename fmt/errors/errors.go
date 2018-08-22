9e6b27a9129b66676bf8d2c95805345e8191f0dc
Roger Peppe <rogpeppe@gmail.com>
2018-08-22 22:29:10 +0100

    fmt/errors: fix stack depth
    We weren't adding the required stack offset.
    Also add some tests to make sure this doesn't happen again.

--- fmt/errors/errors.go
+++ fmt/errors/errors.go
@@ -2,22 +2,33 @@
 // it adds convenience functions that use the fmt package to format
 // error messages.
 package errors
-import "fmt"
+
+import (
+	"fmt"
+
+	"gopkg.in/errgo.v2/errors"
+)
 
 // Newf is like New except it formats the message with a fmt
 // format specifier.
 func Newf(format string, a ...interface{}) error {
-	return New(fmt.Sprintf(format, a...))
+	err := errors.New(fmt.Sprintf(format, a...))
+	errors.SetLocation(err, 1)
+	return err
 }
 
 // Notef is like Note except it formats the message with a fmt
 // format specifier.
 func Notef(err error, shouldPreserveCause func(error) bool, format string, a ...interface{}) error {
-	return Note(err, shouldPreserveCause, fmt.Sprintf(format, a...))
+	err = Note(err, shouldPreserveCause, fmt.Sprintf(format, a...))
+	errors.SetLocation(err, 1)
+	return err
 }
 
 // Becausef is like Because except it formats the message with a fmt
 // format specifier.
 func Becausef(err, cause error, format string, a ...interface{}) error {
-	return Because(err, cause, fmt.Sprintf(format, a...))
+	err = Because(err, cause, fmt.Sprintf(format, a...))
+	errors.SetLocation(err, 1)
+	return err
 }
